8d4f31ebac0a00cb8a0a45565c75e79bc9b423d6
mo9527 <felix_21@163.com>
2018-05-24 19:36:44 +0800

    delete useless code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,20 @@
 var LibraToken = artifacts.require("./LibraToken.sol");
-var AirdropLibraToken = artifacts.require("./AirdropLibraToken.sol");
+var AirdropLibraToken = artifacts.require("./AirdropLibra.sol");
 
 module.exports = function (deployer) {
     const args = process.argv.slice();
 
     console.log('args length: ' + args.length);
-    deployer.deploy(LibraToken).
+    /*deployer.deploy(LibraToken).
         then(function () {
             deployer.deploy(AirdropLibraToken, LibraToken.address, 10000, 1525258592, 1525517792);
-    });
+    });*/
 
-    /*if(args.length < 7){
+    if(args.length < 6){
         console.error('args number error');
 
     }else {
-        deployer.deploy(AirdropLibraToken, args[3], args[4], args[5], args[6]);
-    }*/
+        deployer.deploy(AirdropLibraToken, args[3], args[4], args[5]);
+    }
 
 };
