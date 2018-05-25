2f2286038ba5907e10315809ded3400d7204f97e
mo9527 <felix_21@163.com>
2018-05-25 18:43:15 +0800

    add deploy explain

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -14,6 +14,13 @@ module.exports = function (deployer) {
         console.error('args number error');
 
     }else {
+        /**
+         * args[3] LBA token address
+         * args[4] Airdrop start time
+         * args[5] Airdrop end time
+         *
+         * example: truffle migrate 0xfe5f141bf94fe84bc28ded0ab966c16b17490657,1527161566,1842780766
+         * */
         deployer.deploy(AirdropLibraToken, args[3], args[4], args[5]);
     }
 
